345443ee3f239b53185d9613a020860bce61d7c6
Julian Silden Langlo <langlo94@gmail.com>
2022-05-20 00:39:28 +0200

    Update server code.

--- FileServer/Program.cs
+++ FileServer/Program.cs
@@ -4,30 +4,66 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
 
+var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Files"));
+fileProvider.UsePollingFileWatcher = true;
+
 app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions
 {
     RequestPath = "/Files",
-    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Files")),
+    FileProvider = fileProvider,
 });
 
-app.MapGet("/files.json", () =>
-{
-    return new Folder { Name = "root" };
-});
+
+app.MapGet("/files.json", () => GetFolder(""));
 
 app.Run();
 
+Folder GetFolder(string subpath)
+{
+    var folder = new Folder {
+        Name = subpath.Split("/").Last(),
+        Path = "./Files" + subpath + "/",
+        SubFolders = new(),
+        Files = new (),
+    };
+
+    var content = fileProvider.GetDirectoryContents(subpath);
+
+    // Recursively add Subfolders
+
+    // Add all files in current folder.
+
+    foreach (var file in content)
+    {
+        if (file.IsDirectory)
+        {
+            folder.SubFolders.Add(GetFolder(subpath + "/" + file.Name));
+        }
+        else
+        {
+            folder.Files.Add(new File
+            {
+                Name = file.Name,
+                Path = "./Files" + subpath + "/" + file.Name,
+            });
+        }
+    }
+
+    return folder;
+}
 
 public class Folder
 {
-    public string Name { get; set; }
-    public List<Folder> SubFolders { get; set; }
-    public List<File> Files { get; set; }
+    public string Name { get; init; }
+    public string Path { get; init; }
+    public List<Folder> SubFolders { get; init; }
+    public List<File> Files { get; init; }
 
 }
 
 public class File
 {
-    public string Name { get; set; }
+    public string Name { get; init; }
+    public string Path { get; init; }
 }
